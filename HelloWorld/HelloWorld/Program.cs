a9ae6183c868b859549157e7d7659e83b46563c5
Ralep <ralep1988@gmail.com>
2021-01-11 17:40:47 +0100

    testing around github

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,25 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string aFriend = "Bill";
+            Console.WriteLine(aFriend);
+
+            aFriend = "Maira";
+            Console.WriteLine(aFriend);
+            Console.WriteLine("Hello " + aFriend);
+            Console.WriteLine($"Hello {aFriend}");
+
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+
+
+
         }
     }
 }
